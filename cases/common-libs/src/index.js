42ca3134dd2761d6fa5ddf90273c9e5946a03af3
Tobias Koppers <tobias.koppers@googlemail.com>
2021-02-12 15:49:33 +0100

    add test case for common libs

---
+++ cases/common-libs/src/index.js
@@ -0,0 +1,26 @@
+import "./babel-runtime";
+import "acorn";
+import "classnames";
+import "core-js";
+import * as dateFn from "date-fns";
+import * as dateFnEsm from "date-fns/esm";
+console.log(dateFn, dateFnEsm);
+import "jquery";
+import "lodash";
+import * as lodashEs from "lodash-es";
+console.log(lodashEs);
+import "moment";
+import "react";
+import "react-dom";
+import "redux";
+import * as rxjs from "rxjs";
+console.log(rxjs);
+import * as underscore from "underscore";
+import * as underscoreModules from "underscore/modules";
+console.log(underscore, underscoreModules);
+import { NIL, parse, stringify, v1, v3, v4, v5, validate, version } from "uuid";
+console.log(NIL, parse, stringify, v1, v3, v4, v5, validate, version);
+import "vue";
+import "zone.js";
+
+console.log("Hello World");
