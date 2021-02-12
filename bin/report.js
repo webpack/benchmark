9aa67b97bb9dbed47dd7ab3f019d715cd5b09e8e
Tobias Koppers <tobias.koppers@googlemail.com>
2021-02-12 15:59:33 +0100

    group results by scenario and case

--- bin/report.js
+++ bin/report.js
@@ -17,14 +17,51 @@ const rootDir = resolve(fileURLToPath(import.meta.url), "../..");
 		}))
 	);
 
-	const mergedDiff = {};
-	for (const { case: caseName, scenario, diff } of diffs) {
-		for (const key of Object.keys(diff)) {
-			mergedDiff[`${caseName} ${scenario} ${key}`] = diff[key];
+	const getMergedDiff = ({ caseFilter, scenarioFilter } = {}) => {
+		const mergedDiff = {};
+		for (const { case: caseName, scenario, diff } of diffs) {
+			if (caseFilter && caseName !== caseFilter) continue;
+			if (scenarioFilter && scenario !== scenarioFilter) continue;
+			for (const key of Object.keys(diff)) {
+				let name = key;
+				if (!scenarioFilter) name = `${scenario} ${name}`;
+				if (!caseFilter) name = `${caseName} ${name}`;
+				mergedDiff[name] = diff[key];
+			}
 		}
-	}
+		return mergedDiff;
+	};
 
-	console.log(formatDiffTable(mergedDiff, { colors: true }));
+	const output = (header, filters, options) => {
+		const diff = formatDiffTable(getMergedDiff(filters), {
+			colors: true,
+			...options,
+		});
+		if (diff) {
+			console.log(header);
+			console.log();
+			console.log(diff);
+			console.log();
+		}
+	};
+
+	output("# Summary", {}, { limit: 20 });
+
+	const cases = new Set(diffs.map((d) => d.case));
+	const scenarios = new Set(diffs.map((d) => d.scenario));
+
+	console.log("# By scenario");
+	console.log();
+	for (const scenario of scenarios) {
+		output(`## ${scenario}`, { scenarioFilter: scenario });
+	}
+	console.log();
+
+	console.log("# By case");
+	console.log();
+	for (const caseName of cases) {
+		output(`## ${caseName}`, { caseFilter: caseName });
+	}
 })().catch((err) => {
 	process.exitCode = 1;
 	console.error(err.stack);
