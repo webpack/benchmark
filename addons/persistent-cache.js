fdd78326c994f30ece1e3a4ddfe6e2ea2c04f3fb
Tobias Koppers <tobias.koppers@googlemail.com>
2021-02-18 16:26:23 +0100

    add perisistent cache support for webpack 4, fix production build caching in webpack 4

--- addons/persistent-cache.js
+++ addons/persistent-cache.js
@@ -1,1 +1,8 @@
-export const args = ["--cache-type", "filesystem"];
+export const config = (content) => `${content}
+
+if(!require("webpack").version.startsWith("4")) {
+	module.exports.cache = { type: "filesystem" };
+}
+`;
+
+export const keepBuildCache = true;
