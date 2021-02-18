f0d875079ca811710db74793a82585c47b9c8390
Tobias Koppers <tobias.koppers@googlemail.com>
2021-02-18 20:42:40 +0100

    fix pnp for webpack 4

--- addons/pnp.js
+++ addons/pnp.js
@@ -1,1 +1,23 @@
 export const installOptions = { pnp: true };
+
+export const packageJson = (json) => {
+	Object.assign(json.dependencies, {
+		"pnp-webpack-plugin": "*",
+	});
+	return json;
+};
+
+export const config = (content) => `${content}
+
+if(require("webpack").version.startsWith("4")) {
+	var PnpWebpackPlugin = require("pnp-webpack-plugin");
+
+	module.exports.resolve = module.exports.resolve || {};
+	module.exports.resolve.plugins = module.exports.resolve.plugins || [];
+	module.exports.resolve.plugins.push(PnpWebpackPlugin);
+
+	module.exports.resolveLoader = module.exports.resolveLoader || {};
+	module.exports.resolveLoader.plugins = module.exports.resolveLoader.plugins || [];
+	module.exports.resolveLoader.plugins.push(PnpWebpackPlugin.moduleLoader(module));
+}
+`;
