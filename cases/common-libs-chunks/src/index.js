8e4c83eb78ca005ebbcd07b86c976c5c65ad5b5a
Tobias Koppers <tobias.koppers@googlemail.com>
2021-03-15 16:59:28 +0100

    add common-libs-chunks test case

---
+++ cases/common-libs-chunks/src/index.js
@@ -0,0 +1,23 @@
+import("./babel-runtime");
+import("@material-ui/core");
+import("@material-ui/lab");
+import("@material-ui/icons");
+import("acorn");
+import("classnames");
+import("date-fns");
+import("date-fns/esm");
+import("jquery");
+import("lodash");
+import("lodash-es");
+import("moment");
+import("react");
+import("react-dom");
+import("redux");
+import("rxjs");
+import("underscore");
+import("underscore/modules");
+import("uuid");
+import("vue");
+import("zone.js");
+
+console.log("Hello World");
