8fefc5b41abd67f35c0a15a851327d17b4bc6b61
Tobias Koppers <tobias.koppers@googlemail.com>
2021-05-07 10:41:50 +0200

    add swc

---
+++ addons/swc-env.js
@@ -0,0 +1,24 @@
+import { writeFile, unlink } from "fs/promises";
+
+export const packageJson = (json) => {
+	json.browserslist = "> 0.25%, not dead";
+	Object.assign(json.devDependencies, {
+		"@swc/core": "*",
+		"swc-loader": "*",
+	});
+	return json;
+};
+
+export const config = (content) => `${content}
+
+module.exports.module = module.exports.module || {};
+module.exports.module.rules = module.exports.module.rules || [];
+module.exports.module.rules.unshift({
+	test: /\.(js|tsx?)$/,
+	use: [
+		{
+			loader: "swc-loader"
+		}
+	]
+});
+`;
